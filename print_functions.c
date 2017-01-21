00d5c2b4382c148a21d4e4bcf9ae547ded73c845
Anne <anne.cognet@holbertonschool.com>
2017-01-21 23:38:01 +0000

    change file name, redo pint and pachr

--- print_functions.c
+++ print_functions.c
@@ -48,52 +48,45 @@ void print_string(const stack_t **h, int l)
 	return (count);
 }
 
+
 /**
- * pchar_stack - peek the top of the stack
+ * pchar - peek the top of the stack
  * @h: pointer to dll
  * @l: line number
  * opcode: pchar
  */
-void pchar_stack(const stack_t **h, int l)
+void pint(const stack_t **h, int l)
 {
-	int value;
 
 	if (!h || !*h)
 	{
-		printf("L%d: can't pchar, stack empty\n", l);
+		printf("L%d: can't pint, %s empty\n", l, flag);
 		free_stack(*h);
-		free(command); /*free the extern variable*/
 		exit(EXIT_STATUS);
 	}
 
-	value = (*h)->n;
-	if (value <0 || value > 127)
-		printf("L%d: can't pchar, value out of range\n", l);
-	else
-		printf("%c\n", value);
+	printf("%c\n", (*h)->n);
 }
 
+
+
+
 /**
- * pchar_queue - peek the back of the queue
+ * pchar - peek the top of the stack
  * @h: pointer to dll
  * @l: line number
  * opcode: pchar
  */
-void pchar_queue(const stack_t **h, int l)
+void pchar(const stack_t **h, int l)
 {
 	int value;
 
 	if (!h || !*h)
 	{
-		printf("L%d: can't pchar, queue empty\n", l);
+		printf("L%d: can't pchar, %s empty\n", l, flag);
 		free_stack(*h);
-		free(command); /*free the extern variable*/
 		exit(EXIT_STATUS);
 	}
-
-/*get to the last element*/
-	while ((*h)->next != NULL)
-		*h = (*h)->next;
 
 	value = (*h)->n;
 	if (value <0 || value > 127)
